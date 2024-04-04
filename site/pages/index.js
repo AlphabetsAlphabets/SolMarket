30b204a7a346e9db05c9dabacb0457cfcc1a8348
Yap Jia Hong <yapjiahong-p@proton.me>
2024-04-04 15:35:40 +0800

    feat(app_page): Basic applciation page.

--- site/pages/index.js
+++ site/pages/index.js
@@ -5,6 +5,8 @@ import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import dynamic from 'next/dynamic'
 import Button from '@/components/Button'
 
+import Image from 'next/image'
+
 const inter = Inter({ subsets: ['latin'] })
 
 function Home() {
@@ -24,14 +26,92 @@ function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          APUBCC Sparkathon 2023
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <WalletMultiButton />
+      <div class="flex w-full max-w-screen-lg items-center rounded-lg bg-gray-500 p-3">
+        <div class="flex-grow">
+          <button class="rounded-full bg-indigo-800 p-3">Sol Market</button>
+           by CoKies
+        </div>
+        <div class="flex-1">
+          <button class="p-3 hover:text-sky-400">Home</button>
+        </div>
+        <div class="flex-1">
+          <button class="p-3 hover:text-sky-400">About</button>
+        </div>
+        <div class="flex-1">
+          <button class="p-3 hover:text-sky-400">Application</button>
+        </div>
+        <div class="flex-1">
+          <button class="p-3 hover:text-sky-400">Collaborator</button>
+        </div>
+        <div class="flex-1 text-right">
+          <button class="rounded-full bg-indigo-800 p-3">Register now</button>
+       </div>
+      </div>
+      <div class="grid grid-cols-1 place-content-center">
+        <div class="place-content-center">
+          <strong>NOW AVAILABLE</strong>
+        </div>
+        <div class="place-content-center">
+          <Image
+            src="/dota2.png"
+            width={891}
+            height={314}
+            objectFit="cover"
+            alt="dota2 pic"
+          />
         </div>
       </div>
+
+      <div class="grid grid-cols-1">
+        <div class="place-content-center pb-5">
+          <strong>FEATURED</strong>
+        </div>
+        <div class="grid grid-cols-5">
+          <div class="flex px-8">
+            <Image
+              src="/a-game.png"
+              width={118}
+              height={114}
+              objectFit="cover"
+            />
+          </div>
+          <div class="flex px-8">
+            <Image
+              src="/axie-infinity.png"
+              width={118}
+              height={114}
+              objectFit="cover"
+            />
+          </div>
+          <div class="flex px-8">
+            <Image
+              src="/running-dead.png"
+              width={118}
+              height={114}
+              objectFit="cover"
+            />
+          </div>
+          <div class="flex px-8">
+            <Image
+              src="/rockstar.png"
+              width={118}
+              height={114}
+              objectFit="cover"
+            />
+          </div>
+          <div class="flex px-8">
+            <Image
+              src="/a-game.png"
+              width={118}
+              height={114}
+              objectFit="cover"
+            />
+          </div>
+        </div>
+      </div>
+
+      <hr></hr>
+      <WalletMultiButton />
 
       <div className="relative flex flex-col justify-between place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <h1 className='font-bold'>This is a boilerplate for the APUBCC Sparkathon 2023</h1>
